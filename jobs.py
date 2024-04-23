8504290d83e6beea81ce18d5eb5afec4846b3299
schwittmann <schwittmann@users.noreply.github.com>
2024-04-23 17:33:57 +0000

    output 2

--- jobs.py
+++ jobs.py
@@ -5,7 +5,8 @@ class DummyJob(Job):
         name = "DummyJob"
         description = "Verify working job sync"
     def run(self):
-        self.logger.info("Job output")
+        self.logger.info("Job output 1")
+        self.logger.info("Job output 2")
         return "Success!"
 
 register_jobs(DummyJob)
